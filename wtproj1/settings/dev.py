d87a49d07400831f4a34c49d137338b3e66facf3
wayfrr <wayfrr@gmail.com>
2021-09-02 12:53:38 +0530

    first commit

---
+++ wtproj1/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-m&8)j4i-*=!80xis+xuq@qnfaty35hae%17@ouzp!gzd6-obci'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
